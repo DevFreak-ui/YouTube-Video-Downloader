e2012e8d87b5fa3d127544345003d3a67dc58b59
DevFreak-ui <mirekuprince18@gmail.com>
2021-03-29 09:39:51 -0700

    Deployment dependencies

--- YouTUbeVideoDownloader/urls.py
+++ YouTUbeVideoDownloader/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
